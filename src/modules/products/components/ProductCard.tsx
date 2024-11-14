20d2a311f5233d131c8f055dda922e38aa2d2010
Ale Duarte <duartemattias4@gmail.com>
2024-11-13 23:15:07 -0400

    reafactor design

--- src/modules/products/components/ProductCard.tsx
+++ src/modules/products/components/ProductCard.tsx
@@ -8,13 +8,13 @@ interface Props {
 
 export const ProductCard = ({ product }: Props) => {
     return (
-        <Card classNames={{ base: "border-none", }} isPressable shadow='none'>
+        <Card className="border-none rounded-[3rem] pb-4"  isPressable shadow='sm'>
             <CardBody >
                 <Image
                     className='mx-auto'
                     src='https://png.pngtree.com/png-clipart/20231019/original/pngtree-pizza-png-with-ai-generated-png-image_13357740.png'
                 />
-                <h3 className='text-center text-lg'>{product.name}</h3>
+                <h3 className='text-center text-2xl'>{product.name}</h3>
                 <p className='text-center text-primary-500 font-bold text-2xl'>${product.price}</p>
                 <p className='text-center text-xl'>
                     <span className='text-gray-600 font-semibold'>{product.stock}u.</span> Disponibles
